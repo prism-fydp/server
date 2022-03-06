4149d0c683159472b77ae2e1e0d74c94e2edf4fb
Jeffrey Goldsmith <jeffgold12@gmail.com>
2022-03-06 16:06:13 -0500

    Implement migration functionality

--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.15.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -19,10 +22,12 @@ require (
 	github.com/jackc/pgx/v4 v4.14.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- internal/db/db.go
+++ internal/db/db.go
@@ -3,8 +3,12 @@ package db
 import (
 	"context"
 	"fmt"
-	"os"
+	"log"
+	"path/filepath"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -19,16 +23,39 @@ func createDatabaseURL(dbConfig *DBConfig) string {
 	)
 }
 
+func validateSchema(dbConfig *DBConfig) error {
+	absPath, err := filepath.Abs("migrations")
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.New(
+		fmt.Sprintf("%s%s", "file:///", absPath),
+		createDatabaseURL(dbConfig)+"?sslmode=disable",
+	)
+	if err != nil {
+		return err
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
+
 func CreateDBClient(dbConfig *DBConfig) *pgx.Conn {
 	// Connect to the database
 	conn, err := pgx.Connect(context.Background(), createDatabaseURL(dbConfig))
 	if err != nil {
-		// TODO: setup logging
-		fmt.Println("Error: could not connect to database.")
-		os.Exit(1)
+		log.Fatal("Error: could not connect to database.")
 	}
 
-	// TODO: run migrations before returning the connection
+	err = validateSchema(dbConfig)
+	if err != nil {
+		log.Print("Error: ", err)
+		log.Fatal("Error: could not migrate database")
+	}
 
 	return conn
 }
